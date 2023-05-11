c2dad133798ce3cb48645dbf1920beebef28a058
Kaan <74866163+KaanOzen-mF@users.noreply.github.com>
2023-05-11 11:12:55 +0300

    Digital Business Card Files Added

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Main from "./components/Main.js"
+
+export default function App(){
+    return ( 
+        <div className="container">
+            <Main />
+        </div>
+    )
+}
